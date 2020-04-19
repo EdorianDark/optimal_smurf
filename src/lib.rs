09edf88c85d64acc5e69a8f011ccb7c025b2e026
Dirk Leifeld <leifeld@posteo.de>
2020-04-19 10:53:23 +0200

    add examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+
+pub fn optimal_number() ->i64 {
+    42
+}
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_optimal_number(){
+        assert_eq!(optimal_number(), 42);
     }
 }
